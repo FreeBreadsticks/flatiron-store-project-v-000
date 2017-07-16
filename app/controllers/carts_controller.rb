4ae0792ff3e0129012edc1869a38e8cd72c18093
Erika Hughes <ehughes@knights.ucf.edu>
2017-07-16 02:08:54 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,11 @@
 class CartsController < ApplicationController
+  def show
+    # binding.pry
+    @current_cart = current_user.current_cart
+  end
 
+  def checkout
+    # binding.pry
+    redirect_to cart_path(current_user.current_cart)
+  end
 end
